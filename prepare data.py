82a45c6d8e22ba15817aa5a2eb35c4b2bccfbcdc
antongolubev5 <antongolubev5@yandex.ru>
2022-07-17 20:30:15 +0300

    + обнуление списка с координатами предложений влияло на состав генерируемого датасета

--- prepare data.py
+++ prepare data.py
@@ -1,4 +1,4 @@
-import collections
+from collections import Counter
 
 from sklearn.model_selection import train_test_split
 import spacy
@@ -70,7 +70,8 @@ def check_type_of_entity(entity_pos_start, entity_pos_end, entity, directory_pat
             # проверяем строки файла с разметкой пока не найдем упоминание входящего
             # т.к. в файле может быть грязные данные, проверяем наличие разделителя \t в строке
             if '\t' in sent:
-                if sent.strip().split('\t')[1].split()[1].isdigit() and sent.strip().split('\t')[1].split()[2].isdigit():
+                if sent.strip().split('\t')[1].split()[1].isdigit() and \
+                        sent.strip().split('\t')[1].split()[2].isdigit():
                     if int(sent.strip().split('\t')[1].split()[1]) == entity_pos_start \
                             and int(sent.strip().split('\t')[1].split()[2]) == entity_pos_end \
                             and sent.strip().split('\t')[-1] == entity:
@@ -115,8 +116,12 @@ def create_tsa_dataset():
 
     в качестве нейтральной части нужно отобрать предложения, содержащие сущности без какой-либо разметки отношений (?)
     """
-    directory_path = 'brat/sentiment_dataset'
-    files = list(sorted([file[:-4] for file in os.listdir(directory_path)]))
+
+    # для анализа отбираются объекты, для которых есть и .ann и .txt файлы
+    dir_path = 'brat/sentiment_dataset'
+    files = list(sorted(set([f for f, cnt in Counter([f[:-4] for f in os.listdir(dir_path)]).items() if cnt == 2])))
+    # files = ['04_12_12g', '5807_text']
+    # files = ['04_12_12g']
 
     nlp = spacy.load('ru_core_news_sm')  # sentencizer
 
@@ -142,13 +147,14 @@ def create_tsa_dataset():
     out_label = []
     out_source = []
     out_entity_tag = []
+    out_file = []
 
     for file in tqdm(files):
-        if os.path.isfile(os.path.join(directory_path, file + '.ann')) and os.path.isfile(
-                os.path.join(directory_path, file + '.txt')):
+        if os.path.isfile(os.path.join(dir_path, file + '.ann')) and os.path.isfile(
+                os.path.join(dir_path, file + '.txt')):
 
             # анализ разметки .ann для каждого файла
-            with open(os.path.join(directory_path, file + '.ann')) as f:
+            with open(os.path.join(dir_path, file + '.ann')) as f:
                 for sent in f.readlines():
 
                     # 1 - наличие оценки от автора [author_pos, author_neg]
@@ -156,7 +162,7 @@ def create_tsa_dataset():
                         tag = check_type_of_entity(int(sent.strip().split('\t')[1].split()[1]),
                                                    int(sent.strip().split('\t')[1].split()[2]),
                                                    sent.strip().split('\t')[-1],
-                                                   directory_path,
+                                                   dir_path,
                                                    file).upper()
                         # проверка принадлежности к списку тэгов
                         if tag in entities_types:
@@ -169,7 +175,7 @@ def create_tsa_dataset():
 
                     # 2 - наличие отношения к сущности [positive_to, negative_to], берем 2-ой атрибут
                     if 'POSITIVE_TO' in sent or 'POSITIVE_NEG' in sent:
-                        entity_info = search_entity(directory_path, file, sent.split()[3].split(':')[1])
+                        entity_info = search_entity(dir_path, file, sent.split()[3].split(':')[1])
                         # проверка принадлежности к списку тэгов
                         if entity_info.strip().split()[1] in entities_types:
                             entity.append(entity_info.strip().split('\t')[-1])
@@ -181,12 +187,12 @@ def create_tsa_dataset():
 
                     # 3 - наличие opinion_relates_to, мнение есть, но автора нет
                     if 'OPINION_RELATES_TO' in sent:
-                        arg1_info = search_entity(directory_path, file, sent.split()[-2].split(':')[1])
-                        arg2_info = search_entity(directory_path, file, sent.split()[-1].split(':')[1])
+                        arg1_info = search_entity(dir_path, file, sent.split()[-2].split(':')[1])
+                        arg2_info = search_entity(dir_path, file, sent.split()[-1].split(':')[1])
                         arg2_tag = check_type_of_entity(int(arg2_info.strip().split('\t')[1].split()[1]),
                                                         int(arg2_info.strip().split('\t')[1].split()[2]),
                                                         arg2_info.strip().split('\t')[-1],
-                                                        directory_path,
+                                                        dir_path,
                                                         file).upper()
 
                         # тональность arg1 подходит + arg2 в списке сущностей
@@ -200,7 +206,7 @@ def create_tsa_dataset():
 
             # анализ содержимого .txt файла, в случае, если найдены сущности из файла .ann
             if len(source) > 0:
-                with open(os.path.join(directory_path, file + '.txt')) as f:
+                with open(os.path.join(dir_path, file + '.txt')) as f:
                     # sentencizing
                     f_total_text = f.read()
                     doc = nlp(f_total_text)
@@ -212,22 +218,25 @@ def create_tsa_dataset():
                         sentence_pos_end.append(f_total_text.find(sent) + len(sent))
 
                     # на основании разметки из .ann и sentencized-файла .txt добавляем сэмплы к генерируемому датасету
+                    # в качестве координат сущности в тексте берется его середина
                     for i in range(len(entity)):
                         entity_pos = (entity_pos_end[i] + entity_pos_start[i]) // 2
                         for j in range(len(sentencized_file)):
-                            if sentence_pos_start[j] < entity_pos < sentence_pos_end[j] and entity[i].upper() in \
-                                    sentencized_file[j].upper():
+                            if sentence_pos_start[j] < entity_pos < sentence_pos_end[j] and entity[i].upper() in sentencized_file[j].upper():
                                 out_sentence.append(sentencized_file[j])
                                 out_entity.append(entity[i])
                                 out_label.append(label[i])
                                 out_entity_tag.append(entity_tag[i])
                                 out_source.append(source[i])
+                                out_file.append(file)
 
                 # обнуление списков после обработки каждого файла
                 entity = []
                 entity_tag = []
                 entity_pos_start = []
                 entity_pos_end = []
+                sentence_pos_start = []
+                sentence_pos_end = []
                 label = []
                 source = []
 
@@ -237,12 +246,14 @@ def create_tsa_dataset():
             'entity': out_entity,
             'entity_tag': out_entity_tag,
             'label': out_label,
-            'source': out_source
+            'source': out_source,
+            'file': out_file
         })
 
-    out_df = out_df.drop_duplicates().sort_values(by=['source', 'entity_tag'])
+    # out_df = out_df.drop_duplicates().sort_values(by=['source', 'entity_tag'])
+    out_df = out_df.sort_values(by=['source', 'entity_tag'])
     out_df.to_csv('data/tsa_dataset.csv', sep='\t', index=False)
-    print(collections.Counter(out_df['source']))
+    print(Counter(out_df['source']))
 
 
 if __name__ == '__main__':
